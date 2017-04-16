12612030b4c1fc5647c9bfa6feb02a95f84ee6e2
Slava <horlet@gmail.com>
2017-04-16 19:51:17 +0300

    feature: simple orbiter

---
+++ orbit/orbit.js
@@ -0,0 +1,100 @@
+let SCREEN_WIDTH = window.innerWidth;
+let SCREEN_HEIGHT = window.innerHeight;
+
+let camera, container, scene, renderer, dirLight, orbiter, planet;
+
+init();
+render();
+
+function init () {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    camera = new THREE.PerspectiveCamera(30, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000);
+    camera.position.z = 100;
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    container.appendChild(renderer.domElement);
+    dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(-1, 0, 1).normalize();
+    scene.add(dirLight);
+    window.addEventListener('resize', onWindowResize, false);
+
+    let planetoid = new THREE.SphereGeometry(1, 40, 40, 0, Math.PI * 2, 0, Math.PI * 2);
+    let massMaterial = new THREE.MeshPhongMaterial({emissive: 0x000000, color: 0x990022});
+
+    let orbiterGeom = new THREE.SphereGeometry(.4, 40, 40, 0, Math.PI * 2, 0, Math.PI * 2);
+    let orbiterMaterial = new THREE.MeshPhongMaterial({emissive: 0x000000, color: 0x22cc00});
+
+    planet = new Planet(-.003, 0, 0, 0, planetoid, massMaterial);
+    scene.add(planet.mesh);
+
+    orbiter = new Orbiter(20, 0, 0, orbiterGeom, orbiterMaterial);
+    orbiter.setVector(0,-.3);
+    scene.add(orbiter.mesh);
+}
+
+
+function render () {
+    renderer.render(scene, camera);
+    orbiter.update(planet);
+    requestAnimationFrame(render);
+}
+
+
+function onWindowResize (event) {
+    SCREEN_HEIGHT = window.innerHeight;
+    SCREEN_WIDTH = window.innerWidth;
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+    camera.updateProjectionMatrix();
+}
+
+
+function Planet (gravity, x, y, z, geometry, material) {
+    this.gravity = gravity;
+    this.position = {x, y, z};
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.mesh.position.set(x, y, z);
+
+    this.vector = {
+        x: 0,
+        y: 0
+    }
+}
+
+function Orbiter (x, y, z, geometry, material) {
+    this.position = {x, y, z};
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.mesh.position.set(x, y, z);
+
+    Orbiter.prototype.update = function (planet) {
+        let pX, pY;
+        let a = planet.position.x - this.position.x;
+        let b = planet.position.y - this.position.y;
+
+        let alpha = Math.atan(a/b) * 180 / Math.PI;
+        if (b > 0) alpha -= 180;
+        let beta = 180 - 90 - Math.abs(alpha);
+
+        let alphaRad = alpha * Math.PI/180;
+        let betaRad = beta * Math.PI/180;
+        let gammaRad = 90 * Math.PI /180;
+
+        pX = planet.gravity * Math.sin(alphaRad)/Math.sin(gammaRad);
+        pY = planet.gravity * Math.sin(betaRad)/Math.sin(gammaRad);
+
+        console.log(b);
+
+        this.vector.x += pX;
+        this.vector.y += pY;
+
+        this.mesh.position.x = this.position.x += this.vector.x;
+        this.mesh.position.y = this.position.y += this.vector.y;
+    };
+
+    Orbiter.prototype.setVector = function (x,y) {
+        this.vector = {x,y};
+    }
+}
